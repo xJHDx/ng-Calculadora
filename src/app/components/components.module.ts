66d97001dbaafa290b4806df7f3607d83a535c66
Johnson Torres <44690752+xJHDx@users.noreply.github.com>
2022-06-05 21:25:59 -0500

    ajustes de compoenentes vista

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { BodyComponent } from './body/body.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [BodyComponent, HeaderComponent],
+  declarations: [BodyComponent, HeaderComponent,FooterComponent],
   imports: [CommonModule],
-  exports: [BodyComponent, HeaderComponent],
+  exports: [BodyComponent, HeaderComponent,FooterComponent],
 })
 export class ComponentsModule {}
